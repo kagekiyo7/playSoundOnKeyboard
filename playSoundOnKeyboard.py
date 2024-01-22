d8b1b562051b7971848768397fb174750d2a15ce
エッケザックス <22173829+kagekiyo7@users.noreply.github.com>
2024-01-23 06:39:56 +0900

    Add files via upload

---
+++ playSoundOnKeyboard.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import os
+import json
+import keyboard
+import pygame
+import time
+
+def init():
+    pygame.mixer.init()
+    global seList
+    seList = {}
+    with open(os.path.join(os.path.dirname(__file__), "seList.json")) as file:
+        seListJson = json.load(file)
+    for key, seName in seListJson.items():
+        if seName:
+            seList[key] = {
+                "name": seName,
+                "file": pygame.mixer.Sound(os.path.join(os.path.dirname(__file__), "SE", seName))
+            }
+    print("Preparation is complete. The following files are defined.:")
+    for key, se in seList.items():
+        print(f"key: {key},\tfileName: {se['name']}")
+
+def main():
+    while True:
+        if (keyboard.read_event().event_type != keyboard.KEY_DOWN):
+            continue
+        for key, se in seList.items():
+            if (keyboard.is_pressed(key)):
+                se["file"].play(0)
+
+if __name__ == "__main__":
+    init()
+    main()
